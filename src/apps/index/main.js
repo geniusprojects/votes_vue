308e3508009a0dbcc829646a6d01eadd88269dd6
geniusprojects <geniusprojects@yandex.ru>
2023-12-17 19:53:24 +0700

    Check Polls ago

--- src/apps/index/main.js
+++ src/apps/index/main.js
@@ -6,6 +6,6 @@ import axios from 'axios'
 import CKEditor from '@ckeditor/ckeditor5-vue'
 import '@ckeditor/ckeditor5-build-classic/build/translations/ru'
 
-axios.defaults.baseURL = 'http://api.zoteps.ru'
+axios.defaults.baseURL = 'https://api.zoteps.ru'
 
 createApp(App).use(store).use(router, axios).use(CKEditor).mount('#app')
